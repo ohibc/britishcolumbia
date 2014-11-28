7f4c4fdbaa93df9ccfd974405ca9b8b289c63d85
Ben Best <bbest@nceas.ucsb.edu>
2014-11-28 13:21:41 -0800

    automatically calculate_scores from commit e55d36209fa832828ca3191a0dce6874f29a068a [ci_skip]

--- subcountry2014/calculate_scores.R
+++ subcountry2014/calculate_scores.R
@@ -2,7 +2,7 @@
 suppressWarnings(require(ohicore))
 
 # set working directory to the scenario directory, ie containing conf and layers directories
-setwd('/Users/bbest/github/clip-n-ship/can/subcountry2014')
+setwd('/home/bbest/github/clip-n-ship/can/subcountry2014')
 
 # load scenario configuration
 conf = Conf('conf')
